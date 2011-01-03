7f49debc430281ed1c3f01394cfe2bdbe05a20f7
Yanick Champoux <yanick+cpan@babyl.dyndns.org>
2011-01-03 01:53:43 +0000

    Import of YANICK/Catalyst-Plugin-VersionedURI-0.1.0 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-VersionedURI
    gitpan-cpan-version:      0.1.0
    gitpan-cpan-path:         YANICK/Catalyst-Plugin-VersionedURI-0.1.0.tar.gz
    gitpan-cpan-author:       YANICK
    gitpan-cpan-maturity:     released

---
+++ lib/Catalyst/Controller/VersionedURI.pm
@@ -0,0 +1,82 @@
+package Catalyst::Controller::VersionedURI;
+BEGIN {
+  $Catalyst::Controller::VersionedURI::AUTHORITY = 'cpan:yanick';
+}
+BEGIN {
+  $Catalyst::Controller::VersionedURI::VERSION = '0.1.0';
+}
+# ABSTRACT: Revert Catalyst::Plugin::VersionedURI's munging
+
+
+use strict;
+use warnings;
+
+use Moose;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+after BUILDALL => sub {
+    my $self = shift;
+    my $app = $self->_app;
+
+    my $regex = $app->versioned_uri_regex;
+
+# we catch the old versions too
+eval <<"END";
+sub versioned :Regex('(${regex})v') {
+    my ( \$self, \$c ) = \@_;
+
+    my \$uri = \$c->req->uri;
+
+    \$uri =~ s#(${regex})v.*?/#\$1#;
+
+    \$c->res->redirect( \$uri );
+
+}
+END
+
+};
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Catalyst::Controller::VersionedURI - Revert Catalyst::Plugin::VersionedURI's munging
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+    package MyApp::Controller::VersionedURI;
+
+    use parent 'Catalyst::Controller::VersionedURI';
+
+    1;
+
+=head1 DESCRIPTION
+
+This controller creates actions to catch the 
+versioned uris created by C<Catalyst::Plugin::VersionedURI>.
+
+=head1 SEE ALSO
+
+L<Catalyst::Plugin::VersionedURI>
+
+=head1 AUTHOR
+
+Yanick Champoux <yanick@babyl.dyndns.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Yanick Champoux.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
